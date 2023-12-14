a760f9f50adb879e48b8f52205e63e65e1aaf89f
El houssaine Bakchich <ebakchic@c3r1p1.1337.ma>
2023-12-14 16:55:29 +0100

    intra project

---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,26 @@
+#include "iter.hpp"
+
+template <typename T>
+void    ft_print_arr(T& arr, size_t size) {
+    for (size_t i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    std::cout << "befor increment:\n";
+    ft_print_arr(arr, 5);
+    std::cout << "after increment:\n";
+    iter(arr, 5, &ft_increment);
+    ft_print_arr(arr, 5);
+
+    std::cout << "befor decrement:\n";
+    ft_print_arr(arr, 5);
+    std::cout << "after decrement:\n";
+    iter(arr, 5, &ft_decrement);
+    ft_print_arr(arr, 5);
+
+    return (0);
+}
