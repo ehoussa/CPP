4f325f6b5b5667dcae2f2c0c68dec3fd162a6645
El houssaine Bakchich <ehoussachich@gmail.com>
2023-12-24 14:59:27 +0100

    class done

---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,20 @@
+#ifndef SPAN
+#define SPAN
+#include <vector>
+
+class Span {
+    private:
+        std::vector<int>    vc;
+        unsigned int        sizevc;
+    public:
+                Span();
+                Span(unsigned int sizevc);
+                Span(Span const &copy);
+                ~Span();
+        Span    &operator=(Span const & assign);
+        void    addNumber(unsigned int element);
+        int     shortestSpan() const;
+        int     shortestSpan() const;
+};
+
+#endif
