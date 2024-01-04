a89a421e93708b7ecadd79e0069c3002044d7a90
El houssaine Bakchich <ehoussachich@gmail.com>
2024-01-04 14:56:18 +0100

    done

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+#ifndef MUTANTSTACK_H
+# define MUTANTSTACK_H
+
+# include <stack>
+# include <iostream>
+
+template< typename T> class MutantStack : public std::stack<T> {
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		iterator begin(){return this->c.begin();}
+		iterator end(){return this->c.end();}
+        const_iterator cbegin(){return this->c.cbegin();}
+		const_iterator cend(){return this->c.cend();}
+        reverse_iterator rbegin(){return this->c.rbegin() ;}
+		reverse_iterator rend(){return this->c.rend();}
+        const_reverse_iterator crbegin(){return this->c.crbegin();}
+		const_reverse_iterator crend(){return this->c.crend();}
+};
+
+#endif
